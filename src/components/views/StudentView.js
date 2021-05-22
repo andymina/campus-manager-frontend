a711591e09f2940d6c756fd040fd6d817b62c07a
Rachel Tieu <rachel.tieu02@myhunter.cuny.edu>
2021-05-22 17:17:20 -0400

    added campus object

--- src/components/views/StudentView.js
+++ src/components/views/StudentView.js
@@ -1,13 +1,20 @@
 const StudentView = (props) => {
   const {student} = props;
 
+  let name = student.firstname + " " + student.lastname;
+
   return (
     <div>
       <img src={student.image}/>
-      <h1>{student.name}</h1>
+      <h1>{name}</h1>
       <h3>{student.email}</h3>
       <p>{student.gpa}</p>
 
+      <div className="campus">
+        {student.campus.image}
+        {student.campus.name}
+        {student.campus.description}
+      </div>
     </div>
   );
 
